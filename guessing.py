b36ff0165429fd37c709706390f08bb4f5272b23
mulhern <mulhernac@ab.edu>
2012-04-13 10:27:57 -0400

    Added a test for prompting n times.

--- guessing.py
+++ guessing.py
@@ -1,4 +1,28 @@
+import unittest
+
+
 def prompt_from_terminal(prompt):
     """prompt user for input from terminal with string prompt"""
     while True:
         yield input(prompt)
+
+def prompt_n_times(prompt, n):
+    for answer in prompt_from_terminal(prompt):
+        yield answer
+
+
+class MyTest(unittest.TestCase):
+
+    def test_prompt_n_times(self):
+        self.assertEqual(len(list(prompt_n_times("Guess: ", 0))),
+                         0)
+        self.assertEqual(len(list(prompt_n_times("Guess: ", -1))),
+                         0)
+        self.assertEqual(len(list(prompt_n_times("Guess: ", 1))),
+                         1)
+        self.assertEqual(len(list(prompt_n_times("Guess: ", 2))),
+                         2)
+        return
+        
+
+
