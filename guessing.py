32dd2336f23693dca2cb78dc7bef6f3dbceb6f02
mulhern <mulhernac@ab.edu>
2012-04-13 10:43:34 -0400

    Altered method.

--- guessing.py
+++ guessing.py
@@ -1,4 +1,5 @@
 import unittest
+import itertools
 
 
 def prompt_from_terminal(prompt):
@@ -6,21 +7,22 @@ def prompt_from_terminal(prompt):
     while True:
         yield input(prompt)
 
-def prompt_n_times(prompt, n):
-    for answer in prompt_from_terminal(prompt):
+def prompt_n_times(guesses, n):
+    """yield the first n items in guesses"""
+    for answer in guesses:
         yield answer
 
 
 class MyTest(unittest.TestCase):
 
     def test_prompt_n_times(self):
-        self.assertEqual(len(list(prompt_n_times("Guess: ", 0))),
+        self.assertEqual(len(list(prompt_n_times(itertools.count(), 0))),
                          0)
-        self.assertEqual(len(list(prompt_n_times("Guess: ", -1))),
+        self.assertEqual(len(list(prompt_n_times(itertools.count(), -1))),
                          0)
-        self.assertEqual(len(list(prompt_n_times("Guess: ", 1))),
+        self.assertEqual(len(list(prompt_n_times(itertools.count(), 1))),
                          1)
-        self.assertEqual(len(list(prompt_n_times("Guess: ", 2))),
+        self.assertEqual(len(list(prompt_n_times(itertools.count(), 2))),
                          2)
         return
         
